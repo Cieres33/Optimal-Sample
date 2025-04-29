770f25af669ab73a0ddaf89bbe969545fab898ab
ming324 <167893712+ming324@users.noreply.github.com>
2025-04-29 15:42:10 +0800

    add icon

--- optimal/app/(tabs)/_layout.tsx
+++ optimal/app/(tabs)/_layout.tsx
@@ -1,26 +1,42 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 import {MainHeader, HistoryHeader} from '@/components/header';
+import { Icon, MD3Colors } from 'react-native-paper';
 export default function TabLayout() {
 
   return (
     <Tabs
       screenOptions={{
         headerShown: true,
+        tabBarActiveTintColor: 'blue',
+        tabBarInactiveTintColor: 'gray'
       }}
       initialRouteName='index'>
       <Tabs.Screen
         name="index"
         options={{
           title: 'Input',
-          header: () => <MainHeader />,
+          tabBarIcon: ({ color, size }) => (
+            <Icon
+            source="import"
+            color={MD3Colors.error50}
+            size={20}
+          />
+          ),
         }}
+        
       />
       <Tabs.Screen
         name="explore"
         options={{
           title: 'Result',
-          header: () => <HistoryHeader />,
+          tabBarIcon: ({ color, size }) => (
+            <Icon
+            source="file-document"
+            color={MD3Colors.error50}
+            size={20}
+          />
+          ),
         }}
       />
     </Tabs>
